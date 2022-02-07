045b2567330029bc483ec5b9bb0b652d337f46f7
Daniel Milan <danielmilanvera@gmail.com>
2022-02-07 15:41:37 +0100

    Exe Solution

--- exercises/01-exe-promises/index.js
+++ exercises/01-exe-promises/index.js
@@ -33,16 +33,46 @@ console.log("Waiting for the fake server to reply...");
 // ----- You have to make it work as intended -----
 const serverResponse = new Promise(responseHandler);
 
-serverResponse.then().catch();
+serverResponse
+.then(updateDOMWithData)
+
+
+
+.catch(updateDOMWithError)
+
 
 function updateDOMWithData(data) {
-  const html = ``; // build the html for data here
+ const html1 =` <div class="container">
+  <article class="product">
+    <img src="${data.img}" alt="">
+    <h3>${data.item}</h3>
+    <h3>$ ${data.price}</h3>
+    <h4>Year: ${data.year}</h4>
+  </article>
+</div>`
 
-  // Append the HTML to the DOM here
+const div = document.createElement("div")
+div.innerHTML = html1
+document.body.appendChild(div)
+
+// build the html for data here
+
+  
 }
 
 function updateDOMWithError(error) {
-  const html = ``; // build the html for the error here
+  const html = `<div class="container error">
+  <article class="product">
+    <h1>ERROR</h1>
+    <p>${error}</p>
+    <img src="https://vignette.wikia.nocookie.net/battlefordreamisland/images/f/f1/Roboty_book.png/revision/latest?cb=20190908174044" alt="">
+  </article>
+</div>`
+
+const div = document.createElement("div")
+div.innerHTML = html
+document.body.appendChild(div)
+
 
   // Append the HTML to the DOM here
 }
@@ -50,7 +80,7 @@ function updateDOMWithError(error) {
 function responseHandler(resolveCb, rejectCb) {
   const serverIsUp = Math.random() > 0.5 ? true : false; //this is a trick to get a 50% chance that the server is broken
 
-  const data = {
+ const data = {
     item: "MacBook Pro 16",
     price: 2600,
     year: 2020,
@@ -61,6 +91,11 @@ function responseHandler(resolveCb, rejectCb) {
 
   setTimeout(
     () => {
+      if(serverIsUp){
+       resolveCb(data)
+      }else{
+       rejectCb(error)
+      }
       // Read the "serverIsUp" flag here
       // and handle the promise to dispaly the correct HTML into the DOM here.
     },
@@ -68,3 +103,12 @@ function responseHandler(resolveCb, rejectCb) {
     1000 + Math.random() * 1000 // This is a random waiting time between 1000 and 2000 ms
   );
 }
+
+
+
+
+
+
+
+
+
